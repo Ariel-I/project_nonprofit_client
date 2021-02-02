06434a737a2de185b860ea57c96700f3f2c96c2d
Ariel_Ibarra <arielibarra39@gmail.com>
2021-02-01 22:20:17 -0800

    add missing comma

--- src/containers/categoryForm.js
+++ src/containers/categoryForm.js
@@ -29,7 +29,7 @@ class CategoryForm extends Component {
         this.setState({
             category: {
                 name: ""
-            }
+            },
             loading: false
         })
     }
