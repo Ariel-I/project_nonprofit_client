47e4e7ce9c722eeaa28537b2a6adc461071c8ec6
Ariel_Ibarra <arielibarra39@gmail.com>
2021-02-02 14:12:01 -0800

    add addNonprofit action

--- src/actions/nonprofits.js
+++ src/actions/nonprofits.js
@@ -6,3 +6,18 @@ export const getNonprofits = () => {
         .then(nonprofits => dispatch({type: "FETCH_NONPROFITS", payload: nonprofits}))
     }
 }
+
+export const addNonprofit = nonprofit => {
+    return(dispatch) => {
+        dispatch({type: "ADD_NONPROFIT"})
+        fetch('/nonprofits', {
+            method: 'POST',
+            body: JSON.stringify(nonprofit),
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        }) 
+        .then(resp => resp.json())
+        .then(np => dispatch({type: "CATEGORY_ADDED", payload: np}))
+    }
+}
