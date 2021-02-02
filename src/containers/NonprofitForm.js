09c02d80853b53889a4ac23400f1378fd6be0724
Ariel_Ibarra <arielibarra39@gmail.com>
2021-02-02 14:25:27 -0800

    add handleOnChange

--- src/containers/NonprofitForm.js
+++ src/containers/NonprofitForm.js
@@ -4,9 +4,38 @@ import {addNonprofit} from '../actions/nonprofits'
 
 class NonprofitForm extends Component {
 
+    state = {
+        nonprofit: {
+            name: "",
+            location: "",
+            description: "",
+            contact_info: "",
+            link: ""
+        },
+        loading: false
+    }
+
+    handleOnChange = event => {
+        this.setState({...this.state,
+            nonprofit: {
+                ...this.state.nonprofit,
+                name: event.target.value,
+                location: event.target.value,
+                description: event.target.value,
+                contact_info: event.target.value,
+                link: event.target.value
+            }
+        })
+    }
+
     render(){
         return(
-
+            <div>
+                <form>
+                    <input type="text" onChange={} value={} />
+                    <button type="submit">Add Nonprofit</button>
+                </form>
+            </div>
         )
     }
 }
