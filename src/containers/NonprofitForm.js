4d318d9a380cb30b844a6f5e0ec1e1462341c504
Ariel_Ibarra <arielibarra39@gmail.com>
2021-02-02 14:15:35 -0800

    add npform

---
+++ src/containers/NonprofitForm.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {addNonprofit} from '../actions/nonprofits'
+
+class NonprofitForm extends Component {
+
+    render(){
+        return(
+
+        )
+    }
+}
+
+
+export default NonprofitForm
