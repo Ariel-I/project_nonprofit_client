3f3d687d9d6d73982a7bac4af0f3bc3f3e6c5784
Ariel_Ibarra <arielibarra39@gmail.com>
2021-02-02 14:34:12 -0800

    add handleOnSubmit

--- src/containers/NonprofitForm.js
+++ src/containers/NonprofitForm.js
@@ -28,11 +28,34 @@ class NonprofitForm extends Component {
         })
     }
 
+    handleOnSubmit = event => {
+        event.preventDeafult();
+        const nonprofit = {...this.state.nonprofit, categoryId: this.props.categoryId}
+        console.log(nonprofit)
+        this.props.addNonprofit(nonprofit)
+        this.setState({
+            nonprofit: {
+                name: "",
+                location: "",
+                description: "",
+                contact_info: "",
+                link: ""
+            }
+            loading: false
+        })
+    }
+
+
     render(){
         return(
             <div>
-                <form>
-                    <input type="text" onChange={} value={} />
+                <form onSubmit={this.handleOnSubmit}>
+                    <input type="text" onChange={this.handleOnChange} value={this.state.nonprofit.name} />
+                    <input type="text" onChange={this.handleOnChange} value={this.state.nonprofit.location} />
+                    <input type="text" onChange={this.handleOnChange} value={this.state.nonprofit.description} />
+                    <input type="text" onChange={this.handleOnChange} value={this.state.nonprofit.contact_info} />
+                    <input type="text" onChange={this.handleOnChange} value={this.state.nonprofit.link} />
+
                     <button type="submit">Add Nonprofit</button>
                 </form>
             </div>
