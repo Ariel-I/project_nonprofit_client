6b52ff06f61afed8d5703ed821df916fa7c68984
Ariel_Ibarra <arielibarra39@gmail.com>
2021-02-02 14:03:39 -0800

    add mapStateToProps

---
+++ src/containers/NonprofitContainer.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {getNonprofits} from '../actions/nonprofits'
+//import NonprofitForm from ''
+
+class NonProfit extends Component {
+
+    render(){
+
+        const nonprofitCards = this.props.nonprofits.map(np => <li key={np.id}>{np.name}</li>)
+
+        return(
+            <div>
+                <ul>
+                    {nonprofitCards}
+                </ul>
+                <Nonprofits />
+            </div>
+        )
+    }
+
+}
+
+const mapStateToProps = state => {
+    console.log("I am state", state)
+    return {
+        nonprofits: state.nonprofitReducer.nonprofits,
+        loading: state.nonprofitReducer.loading
+    }
+}
+
+export default connect(mapStateToProps, {getNonprofits})(NonProfit)
