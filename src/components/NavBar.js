4592913db90ba95013f80746cacde981e403b571
Ariel_Ibarra <arielibarra39@gmail.com>
2021-02-02 21:34:59 -0800

    change class to className

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react'
-import { Link } from 'react-router-dom'
+//import { Link } from 'react-router-dom'
 
 class NavBar extends Component {
     render(){
         return(
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="/">PROJECT NONPROFIT</a>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">PROJECT NONPROFIT</a>
                 </div>
             </nav>
         )
