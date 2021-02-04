a0a5b8402bb81f436eca1a91c11b80e887b61c6f
Ariel_Ibarra <arielibarra39@gmail.com>
2021-02-03 18:29:35 -0800

    remove CategoryContainer/place logic in App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,60 @@
 import React, {Component} from 'react';
 import './App.css';
-//import CategoryContainer from './containers/CategoryContainer';
 //import CategoryForm from './containers/CategoryForm';
 import NonprofitContainer from './containers/NonprofitContainer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 import Home from './components/Home';
+import {connect} from 'react-redux'
+import {getCategories} from './actions/categories'
 import Category from './components/categories/Category'
 import Nonprofit from './components/nonprofits/Nonprofit'
 
-
 class App extends Component {
+
+  componentDidMount(){
+    this.props.getCategories()
+  }
 
   render(){
 
+    const categorySelect = this.props.categories.map(category => 
+     <li><Link to='/categories/:id/nonprofits' key={category.id}>{category.name}</Link></li>)
+
     return (
-      <div className="App ">
+      <div>
         <Router>
           <NavBar />
-          
+         
           <Switch>
             <Route exact path='/' component={Home} />
-            
+
             <Route exact path="/categories/:id/nonprofits" component={Category} />
             <Route exact path="/categories/:id/nonprofits/:id" component={Nonprofit} />
             <Route exact path="/nonprofits" component={NonprofitContainer} />
             <hr/>
-            
           </Switch>
+          <div className="container">
+            <h2>Choose A Category: </h2>
+            <ul>
+              {categorySelect}
+            </ul>
+          </div>
         </Router>
+             
+
       </div>
     )
   }
 }
  
-export default App;
+const mapStateToProps = state => {
+  //console.log("I am the state", state)
+  return {
+    categories: state.categoryReducer.categories,
+    loading: state.categoryReducer.loading
+  }
+}
+
+export default connect(mapStateToProps, {getCategories})(App);
